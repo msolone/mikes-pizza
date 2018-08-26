572dbecaa3ab65ab9d0997c306229125e8873e3b
Michael Solone <michaelsolone@gmail.com>
2018-08-26 15:40:53 -0400

    right side functions as intended

--- src/App.js
+++ src/App.js
@@ -2,39 +2,78 @@ import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CategoriesList from "./Components/CategoriesList";
-import FoodList from './Components/FoodList';
+import FoodList from "./Components/FoodList";
 import Data from "./Data.json";
 import FoodDetail from "./Components/FoodDetail";
+import Pagination from "react-js-pagination";
 
+
+// const ALPHABET = ['a','c','d','e','f','g','h','i','j','k']
+// const PAGE_SIZE = 2
 
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     activePages: 15,
+  //     currentPage: [],
+  //     activePage:0
+  //   };
+  // }
+
+//   componentDidMount () {
+//     this.setState({
+//       currentPage: Data['appetizers'].meals.slice(0,PAGE_SIZE)
+//     })
+//   }
+
+//   handlePageChange = (pageNumber) => {
+//     console.log(`active page is ${pageNumber}`);
+//     const last = pageNumber * PAGE_SIZE;
+//     const first = last - PAGE_SIZE;
+//     const current = Data['appetizers'].meals.slice(first, last);
+//     console.log({current, first, last})
+//     this.setState({ activePage: pageNumber, currentPage:current });
+//   }
+
   render() {
-    // console.log(Data.drinks.meals[0]);
+    console.log(Data['appetizers'].meals)
     return (
       <Router>
         <section className="perma-page">
           <section className="header">
             <h1>Mike's Pizza</h1>
+           {/* current:page: {this.state.activePage}
+            {this.state.currentPage.map((item, i) => {
+              return <div key={i}>{item}</div>
+            })}
+            <Pagination
+              activePage={this.state.activePage}
+              itemsCountPerPage={PAGE_SIZE}
+              totalItemsCount={Data['appetizers'].meals.length}
+              pageRangeDisplayed={2}
+              onChange={this.handlePageChange}
+            /> */}
           </section>
           <section className="body">
             <section className="categories">
               <ul className="left-side-bar">
-                <Link to="./appetizers">
+                <a href="http://localhost:3000/appetizers">
                   <li>{Data.appetizers.title}</li>
-                  <img src={Data.appetizers.meals[1].imageURL} alt='food' />
-                </Link>
-                <Link to="./entrees">
+                  <img src={Data.appetizers.meals[1].imageURL} alt="food" />
+                </a>
+                <a href="http://localhost:3000/entrees">
                   <li>{Data.entrees.title}</li>
-                  <img src={Data.entrees.meals[1].imageURL} alt='food' />
-                </Link>
-                <Link to="./desserts">
+                  <img src={Data.entrees.meals[1].imageURL} alt="food" />
+                </a>
+                <a href="http://localhost:3000/desserts">
                   <li>{Data.desserts.title}</li>
-                  <img src={Data.desserts.meals[1].imageURL} alt='food' />
-                </Link>
-                <Link to="./drinks">
+                  <img src={Data.desserts.meals[1].imageURL} alt="food" />
+                </a>
+                <a href="http://localhost:3000/drinks">
                   <li>{Data.drinks.title}</li>
-                  <img src={Data.drinks.meals[1].imageURL} alt='food' />
-                </Link>
+                  <img src={Data.drinks.meals[1].imageURL} alt="food" />
+                </a>
               </ul>
             </section>
 
@@ -50,19 +89,41 @@ class App extends Component {
               <ul className="right-side-bar">
                 <Link to="">
                   <li>Deal #1</li>
-                  <img src={Data.appetizers.meals[Math.floor(Math.random() * 4)].imageURL} alt='food' />
+                  <img
+                    src={
+                      Data.appetizers.meals[Math.floor(Math.random() * 4)]
+                        .imageURL
+                    }
+                    alt="food"
+                  />
                 </Link>
                 <Link to="">
                   <li>Deal #2</li>
-                  <img src={Data.entrees.meals[Math.floor(Math.random() * 4)].imageURL} alt='food' />
+                  <img
+                    src={
+                      Data.entrees.meals[Math.floor(Math.random() * 4)].imageURL
+                    }
+                    alt="food"
+                  />
                 </Link>
                 <Link to="">
                   <li>Deal #3</li>
-                  <img src={Data.desserts.meals[Math.floor(Math.random() * 4)].imageURL} alt='food' />
+                  <img
+                    src={
+                      Data.desserts.meals[Math.floor(Math.random() * 4)]
+                        .imageURL
+                    }
+                    alt="food"
+                  />
                 </Link>
                 <Link to="">
                   <li>Deal #4</li>
-                  <img src={Data.drinks.meals[Math.floor(Math.random() * 4)].imageURL} alt='food' />
+                  <img
+                    src={
+                      Data.drinks.meals[Math.floor(Math.random() * 4)].imageURL
+                    }
+                    alt="food"
+                  />
                 </Link>
               </ul>
             </section>
